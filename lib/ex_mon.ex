07463388b0ca48f724113d82f358b79f03b3ce1a
Leonardo Flores <contato@leonardocouy.com>
2022-04-13 11:13:06 -0300

    Add logic to start game

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,18 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+  alias ExMon.Game.Status
+
+  @computer_name "Crazy Rooster"
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+    @computer_name
+      |> create_player(:punch, :kick, :heal)
+      |> Game.start(player)
+
+    Status.print_round_message()
   end
 end
