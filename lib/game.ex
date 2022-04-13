308ced867f3aca3a66de25259d69c48d5c597101
Leonardo Flores <contato@leonardocouy.com>
2022-04-13 11:59:44 -0300

    Implement attack logic

--- lib/game.ex
+++ lib/game.ex
@@ -14,5 +14,11 @@ defmodule ExMon.Game do
 
   def info, do: Agent.get(__MODULE__, & &1)
 
+  def update(state) do
+    Agent.update(__MODULE__, fn _ -> state end)
+  end
+
   def player, do: Map.get(info(), :player)
+  def turn, do: Map.get(info(), :turn)
+  def fetch_player(player), do: Map.get(info(), player)
 end
