c2860f26572566e9743ea7409bd4eace02bc202c
Leonardo Flores <contato@leonardocouy.com>
2022-04-14 14:08:14 -0300

    Add tests for Player module

---
+++ test/player_test.exs
@@ -0,0 +1,19 @@
+defmodule ExMon.PlayerTest do
+  use ExUnit.Case
+
+  alias ExMon.Player
+
+  describe "build/4" do
+    test "returns a Player struct" do
+      assert Player.build("Leo", :kick, :punch, :heal) == %Player{
+        life: 100,
+        moves: %{
+          move_avg: :punch,
+          move_heal: :heal,
+          move_rnd: :kick
+        },
+        name: "Leo"
+      }
+    end
+  end
+end
